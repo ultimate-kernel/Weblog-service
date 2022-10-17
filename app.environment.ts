f1fc3c36d50c931eba652a15358c61933ef939c2
aaronlamz <aaronlamz2022@gmail.com>
2022-10-17 21:45:03 +0800

    add env

---
+++ app.environment.ts
@@ -0,0 +1,6 @@
+export const environment = process.env.NODE_ENV;
+export const isDevEnv = Object.is(environment, 'development');
+export const isProdEnv = Object.is(environment, 'production');
+export const isTestEnv = Object.is(environment, 'test');
+
+export { isDevEnv, isProdEnv, isTestEnv, environment };
