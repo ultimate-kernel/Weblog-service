4b9be42b8dd8acb8110f051ef884108a803def52
aaronlamz <aaronlamz2022@gmail.com>
2022-10-20 10:05:21 +0800

    update

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,8 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { ArticlePaginateQueryDTO } from './artitle.dto';
+import { ArticleService } from './article.service';
 
 @Controller('article')
 export class ArticleController {
+  constructor(private readonly artitleService: ArticleService) {}
+
   @Get()
   async getArticles(@Query query: ArticlePaginateQueryDTO) {
     console.log(query);
