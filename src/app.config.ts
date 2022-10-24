b96d6739c1baa61d8d080719592adf47c6393644
aaronlamz <aaronlamz2022@gmail.com>
2022-10-24 20:38:38 +0800

    update

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,13 +6,13 @@ const ROOT_PATH = path.join(__dirname, '..');
 
 const pkg = require(path.resolve(ROOT_PATH, 'package.json'));
 
-export const APP_CONFIG = {
+export const APP = {
   PORT: 8888,
   NAME: 'WeblogService',
   ROOT_PATH,
 };
 
-export const PROJECT_INFO = {
+export const PROJECT = {
   name: pkg.name,
   version: pkg.version,
   author: pkg.author,
@@ -21,11 +21,11 @@ export const PROJECT_INFO = {
   repository: pkg.repository.url,
 };
 
-export const MONGODB_CONFIG = {
+export const MONGO_DB = {
   url: argv.db_url || `mongodb://127.0.0.1:27017/WeblogService`,
 };
 
-export const REDIS_CONFIG = {
+export const REDIS = {
   host: argv.redis_host || 'localhost',
   port: argv.redis_port || 6379,
   username: argv.redis_username || null,
--- src/processors/database/database.provider.ts
+++ src/processors/database/database.provider.ts
@@ -1,3 +1,14 @@
 import mongoose from 'mongoose';
+import { DB_CONNECTION_TOKEN } from '@app/constants/system.constant';
+import * as APP_CONFIG from '@app/app.config';
 
-export const databaseProvider = {};
+// Factory providers more details: https://docs.nestjs.cn/8/fundamentals?id=%e5%b7%a5%e5%8e%82%e6%8f%90%e4%be%9b%e8%80%85-usefactory
+export const databaseProvider = {
+  provide: DB_CONNECTION_TOKEN,
+  useFactory: async () => {
+    const connection = () => {
+      return mongoose.connect(APP_CONFIG.MONGO_DB.url, {});
+    };
+    return await connection();
+  },
+};
