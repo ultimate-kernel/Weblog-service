1fdab1a27eeb054ca16fdb62ccfe946a7c83e8fb
aaronlamz <aaronlamz2022@gmail.com>
2022-10-24 20:48:32 +0800

    add logger

--- src/processors/database/database.provider.ts
+++ src/processors/database/database.provider.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import { DB_CONNECTION_TOKEN } from '@app/constants/system.constant';
 import * as APP_CONFIG from '@app/app.config';
+import logger from '@app/utils/logger';
+
+const log = logger.scope('MongoDB');
 
 // Factory providers more details: https://docs.nestjs.cn/8/fundamentals?id=%e5%b7%a5%e5%8e%82%e6%8f%90%e4%be%9b%e8%80%85-usefactory
 export const databaseProvider = {
@@ -9,6 +12,24 @@ export const databaseProvider = {
     const connection = () => {
       return mongoose.connect(APP_CONFIG.MONGO_DB.url, {});
     };
+
+    mongoose.connection.on('connecting', () => {
+      log.info('connecting...');
+    });
+
+    mongoose.connection.on('open', () => {
+      log.info('connection open');
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      log.error(`disconnected!`);
+    });
+
+    mongoose.connection.on('error', (error) => {
+      log.error('error!', error);
+      mongoose.disconnect();
+    });
+
     return await connection();
   },
 };
